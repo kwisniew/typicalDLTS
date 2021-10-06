c2a4e205bcb3ba634233aca811c5d2945ea24a67
kwisniew <kon.wu888@gmail.com>
2021-10-06 18:30:22 +0200

    rename

--- emissionRate.m
+++ emissionRate.m
@@ -1,4 +1,4 @@
-function emmisionRate = emmisionRate(sig,v0,Nc,Ea,T)
+function emmisionRate = emissionRate(sig,v0,Nc,Ea,T)
     k   = 8.62*10^-5; %eV/K;
     emmisionRate = sig*v0*Nc*exp(-Ea/(k*T)); %s^-1
 end
